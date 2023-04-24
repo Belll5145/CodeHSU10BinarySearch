226a89be71157b2c6f744dcf2742d04a1f726cf1
Belll5145 <Belll@wlhs.wlwv.k12.or.us>
2023-04-24 09:19:38 -0700

    Did binary and linear search

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Arrays;
 
 
 public class Main {
@@ -9,26 +10,46 @@ public class Main {
             myNumList[i] = (int)(Math.random()*(100)+1);
         }
 
-
+        Arrays.sort(myNumList);
         int randIndex = (int)(Math.random()*(100)+1);
         int randNumToFind = myNumList[randIndex];
 
         //linear search
-
-
-        //binary search
-
         long start1 = System.currentTimeMillis();
+        int location = linearSearch(myNumList, randNumToFind);
         long end1 = System.currentTimeMillis();
         System.out.println();
         System.out.println("Elapsed time in ms: " + (end1 - start1));
+        //binary search
+        long start2 = System.currentTimeMillis();
+        int location2 = binarySearch(myNumList, randNumToFind, 0, myNumList.length - 1);
+        long end2 = System.currentTimeMillis();
+        System.out.println();
+        System.out.println("Elapsed time in ms: " + (end1 - start1));
+
     }
 
-    public static boolean linearSearch(int[] array, int target){
-        return false;
+    public static int linearSearch(int[] array, int target){
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == target){
+                return i;
+            }
+        }
+        return Integer.MAX_VALUE;
     }
 
-    public static boolean binarySearch(int[] array, int target){
-        return false;
+    public static int binarySearch(int[] array, int target, int start, int end){
+        int mid = (start + end)/2;
+        if(target == array[mid]){
+            return mid;
+        }
+        if(target < array[mid]){
+            return binarySearch(array, target, start, mid-1);
+        }
+        if(target > array[mid]){
+            return binarySearch(array,target,mid+1,end);
+
+            }
+        return Integer.MAX_VALUE;
+        }
     }
-}
